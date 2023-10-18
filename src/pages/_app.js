e67333bfe2d40fc91c32660c6179a28fbcd2a01b
Abdurahmon086 <1937ali047@gmail.com>
2023-10-17 19:02:31 -0700

    loading optimize

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,18 +1,22 @@
 import Layout from '@/components/Layout/layout'
 import '@/styles/globals.css'
 import Link from 'next/link'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 
 export default function App({ Component, pageProps }) {
 
   const [load, setLoad] = useState(true)
-  setTimeout(() => {
-    setLoad(false)
-  }, 500)
-  console.log(pageProps)
+
+  useEffect(() => {
+    setTimeout(() => {
+      setLoad(false)
+    }, 500)
+
+  }, [])
+
   return (
     <>
-      {load == true ?
+      {load ?
         <div className='h-[100vh] bg-blue-50 flex justify-center items-center '>
           <div className="loader mt-[-50px]"></div>
         </div>
